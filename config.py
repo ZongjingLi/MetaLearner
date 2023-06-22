f63c0811e4643a6521fa67868fc7974cbaa95d16
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-06-22 14:18:40 +0800

    deliver again

--- config.py
+++ config.py
@@ -2,6 +2,10 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
+# language model configuration
+parser.add_argument("--corpus_path",            default = "assets/corpus.txt")
+
+# reasoning model configuration
 parser.add_argument("--box_dimension")
 
 config = parser.parse_args(args = [])
