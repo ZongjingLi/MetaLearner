6299cdc15295bf7ce4ef556f1235bd5b129047c0
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-06-22 14:22:22 +0800

    Green Manalishi

--- config.py
+++ config.py
@@ -4,6 +4,9 @@ parser = argparse.ArgumentParser()
 
 # language model configuration
 parser.add_argument("--corpus_path",            default = "assets/corpus.txt")
+parser.add_argument("--num_words",              default = int(1e5))
+parser.add_argument("--word_dim",               default = 132)
+parser.add_argument("--semantics_dim",          default = 232)
 
 # reasoning model configuration
 parser.add_argument("--box_dimension")
