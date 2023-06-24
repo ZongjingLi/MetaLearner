24ff275c188a1be0678819e205e50fdbd51909d1
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-06-24 10:48:05 +0800

    0

---
+++ debug.py
@@ -0,0 +1,16 @@
+from models import *
+from config import *
+
+langs = LanguageModel(config)
+metar = MetaReasoner(config)
+
+output_seqs = langs([
+    "what is that ?",
+    "what is Banach-Steinhaus theorem ?",
+    "what is that concept ?"])
+
+print(metar.executor.parse("filter(scene(),banach_space)"))
+print(metar.executor.get_concept_embedding("banach_space").shape)
+
+for s in output_seqs["seq_features"]:
+    print(s.shape)
