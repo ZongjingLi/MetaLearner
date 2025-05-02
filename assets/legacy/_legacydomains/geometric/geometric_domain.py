b9e88d3e8914d45fd27352daf2de85568fbf5200
zongjingli <lizongjing997@gmail.com>
2025-05-01 20:09:26 -0400

    remove the legacy methods

--- assets/legacy/_legacydomains/geometric/geometric_domain.py
+++
@@ -1,7 +0,0 @@
-'''
- # @ Author: Zongjing Li
- # @ Create Time: 2024-11-19 01:12:51
- # @ Modified by: Zongjing Li
- # @ Modified time: 2024-11-19 01:13:06
- # @ Description: This file is distributed under the MIT license.
-'''
--- assets/legacy/experiments/near-construct.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Author: zongjingli
-# @Date:   2025-01-23 18:04:33
-# @Last Modified by:   zongjingli
-# @Last Modified time: 2025-02-02 12:38:57
